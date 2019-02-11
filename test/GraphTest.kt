adc4c31a85025180b748159f798bb886aa9def4e
triadecom <triadeaic@gmail.com>
2019-02-11 12:58:05 +0300

    Added basic JUnit 4 tests structure

---
+++ test/GraphTest.kt
@@ -0,0 +1,26 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GraphTest {
+
+    @Test
+    fun addHeight() {
+    }
+
+    @Test
+    fun deleteHeight() {
+    }
+
+    @Test
+    fun getOutVerticles() {
+    }
+
+    @Test
+    fun getInVerticles() {
+    }
+
+    @Test
+    fun getGraphList() {
+    }
+}
